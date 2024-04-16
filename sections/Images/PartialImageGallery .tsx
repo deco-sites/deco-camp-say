66012ecd05f6c1ceda284cb25afc3618065e3335
Saymon Damásio <saymon.damasio95@gmail.com>
2024-04-16 18:45:05 -0300

    task 3

--- sections/Images/PartialImageGallery .tsx
+++ sections/Images/PartialImageGallery .tsx
@@ -1,5 +1,7 @@
 import { ImageWidget } from "apps/admin/widgets.ts";
 import { usePartialSection } from "deco/hooks/usePartialSection.ts";
+import Image from "apps/website/components/Image.tsx";
+import Button from "deco-sites/deco-camp-say/components/ui/Button.tsx";
 
 export interface Props {
   images: ImageWidget[];
@@ -10,25 +12,31 @@ export default function PartialImageGallery(
   { images, quantRenderImages = 3 }: Props,
 ) {
   return (
-    <div>
-      {images.slice(0, quantRenderImages).map((image, index) => (
-        <img src={image} alt={image} class="w-28" />
-      ))}
+    <div class="mx-auto max-w-[865px]">
+      <div class="flex flex-wrap gap-3">
+        {images.slice(0, quantRenderImages).map((image, index) => (
+          <Image
+            src={image}
+            width={280}
+            height={190}
+            class="rounded-xl hover:scale-110 transition"
+          />
+        ))}
+      </div>
 
       {quantRenderImages < images.length && (
-        <div>
-          <button
-            {...usePartialSection({
-              mode: "replace",
-              props: {
-                images,
-                quantRenderImages: quantRenderImages + 1,
-              },
-            })}
-          >
-            Veja mais
-          </button>
-        </div>
+        <Button
+          class="mx-auto mt-3 bg-primary block"
+          {...usePartialSection({
+            mode: "replace",
+            props: {
+              images,
+              quantRenderImages: quantRenderImages + 1,
+            },
+          })}
+        >
+          Veja mais
+        </Button>
       )}
     </div>
   );
--- sections/Product/HorizontalProductCard.tsx
+++ sections/Product/HorizontalProductCard.tsx
@@ -3,12 +3,24 @@ import Image from "apps/website/components/Image.tsx";
 import { ProductDetailsPage } from "apps/commerce/types.ts";
 import Button from "deco-sites/deco-camp-say/components/ui/Button.tsx";
 import { useOffer } from "deco-sites/deco-camp-say/sdk/useOffer.ts";
+import AddToCartButtonVTEX from "../../islands/AddToCartButton/vtex.tsx";
 import { formatPrice } from "deco-sites/deco-camp-say/sdk/format.ts";
 import { AppContext } from "deco-sites/deco-camp-say/apps/site.ts";
 import { SectionProps } from "deco/types.ts";
+import { clx } from "deco-sites/deco-camp-say/sdk/clx.ts";
 
 export interface Props {
   productPage: ProductDetailsPage;
+  maxWidth?:
+    | "max-w-xl"
+    | "max-w-2xl"
+    | "max-w-3xl"
+    | "max-w-4xl"
+    | "max-w-5xl"
+    | "max-w-6xl"
+    | "max-w-7xl"
+    | "max-w-full";
+  animateImage?: boolean;
 }
 
 export const loader = (props: Props, _req: Request, ctx: AppContext) => {
@@ -19,26 +31,70 @@ export const loader = (props: Props, _req: Request, ctx: AppContext) => {
 };
 
 export default function HorizontalProductCard(
-  { productPage: { product } }: SectionProps<typeof loader>,
+  { productPage: { product }, maxWidth = "max-w-5xl", animateImage = true }:
+    SectionProps<
+      typeof loader
+    >,
 ) {
   const mainImage = product?.image?.[0];
 
-  const { price } = useOffer(product.offers);
+  const { offers, productID, url, name } = product;
+
+  const { price, listPrice, seller } = useOffer(product.offers);
+  const eventParams = {
+    items: [{ item_url: url, quantity: 1, item_name: name! }],
+  };
 
   return (
-    <div>
-      <Image src={mainImage?.url!} width={300} loading={"lazy"} />
-      <div>
-        <h2>{product?.name}</h2>
-        <p>{product?.description}</p>
-        <div class="flex lg:hidden">
-          <strong>{formatPrice(price)}</strong>
-          <Button href={product?.url}>Comprar</Button>
+    <div
+      class={clx(
+        "card bg-base-100 p-5 overflow-hidden shadow-xl mx-auto flex flex-row gap-2",
+        maxWidth,
+      )}
+    >
+      <div class="overflow-hidden">
+        <Image
+          src={mainImage?.url!}
+          width={200}
+          height={280}
+          loading={"lazy"}
+          class={clx(
+            "aspect-square h-full",
+            animateImage && "hover:scale-110 transition",
+          )}
+        />
+      </div>
+      <div class="flex flex-col gap-6 text-neutral-600">
+        <h2 class="text-lg lg:text-2xl line-clamp-1">{product?.name}</h2>
+        <p class="text-base lg:text-xl line-clamp-1">{product?.description}</p>
+        <div class="flex flex-col items-start sm:flex-row sm:items-center gap-3 lg:hidden">
+          <div class="flex flex-col">
+            <div class="line-through text-xs font-light">
+              {formatPrice(listPrice, offers?.priceCurrency)}
+            </div>
+            <div class="text-base-content lg:text-sm font-bold">
+              {formatPrice(price, offers?.priceCurrency)}
+            </div>
+          </div>
+          <AddToCartButtonVTEX
+            eventParams={eventParams}
+            productID={productID}
+            seller={seller ?? "1"}
+          />
         </div>
       </div>
-      <div class="hidden lg:block">
-        <strong>{formatPrice(price)}</strong>
-        <Button href={product?.url}>Comprar</Button>
+      <div class="hidden lg:flex ml-auto flex-col gap-2">
+        <div class="line-through text-xs font-light">
+          {formatPrice(listPrice, offers?.priceCurrency)}
+        </div>
+        <div class="text-base-content lg:text-sm font-bold">
+          {formatPrice(price, offers?.priceCurrency)}
+        </div>
+        <AddToCartButtonVTEX
+          eventParams={eventParams}
+          productID={productID}
+          seller={seller ?? "1"}
+        />
       </div>
     </div>
   );
@@ -60,19 +116,30 @@ export function ErrorFallback({ error }: { error?: Error }) {
 
 export function LoadingFallback() {
   return (
-    <div>
-      <div class="skeleton w-32 h-32"></div>
-      <div>
-        <div class="skeleton h-4 w-20"></div>
-        <div class="skeleton h-4 w-20"></div>
-        <div class="flex lg:hidden">
-          <div class="skeleton h-4 w-20"></div>
-          <div class="skeleton h-4 w-20"></div>
+    <div
+      class={clx(
+        "card max-w-5xl p-5 shadow-xl mx-auto flex flex-row gap-2",
+      )}
+    >
+      <div class="overflow-hidden">
+        <div class="skeleton w-[200px] h-[280px]"></div>
+      </div>
+      <div class="flex w-full max-w-96 flex-col gap-6 text-neutral-600">
+        <div class="skeleton h-5 w-full"></div>
+        <div class="skeleton h-20 w-full"></div>
+        <div class="flex flex-col items-start sm:flex-row sm:items-center gap-3 lg:hidden">
+          <div class="flex flex-col">
+            <div class="skeleton h-5 w-full max-w-28"></div>
+            <div class="skeleton h-5 w-full max-w-28"></div>
+          </div>
+          <div class="skeleton h-11 w-full"></div>
         </div>
       </div>
-      <div class="hidden lg:block">
-        <div class="skeleton h-4 w-20"></div>
-        <div class="skeleton h-4 w-20"></div>
+      <div class="hidden max-w-32 w-full lg:flex ml-auto flex-col gap-2">
+        <div class="skeleton h-5 w-full max-w-28"></div>
+        <div class="skeleton h-5 w-full max-w-28"></div>
+
+        <div class="skeleton h-11 w-full"></div>
       </div>
     </div>
   );
